5c245e6b0fe9554995c749a12bf56e48d620df6b
MücahitSeba <mucahitseba@gmail.com>
2018-12-12 15:41:04 +0300

    Classlar eklendi

---
+++ Classlar.Lib/Calısan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar.Lib
+{
+    public class Calısan : Kisi, IMaasAlan
+    {
+        public decimal SaatlikUcret { set => throw new NotImplementedException(); }
+
+        public decimal Tutar => throw new NotImplementedException();
+
+        public decimal AvansVer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal OdemeYap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Classlar.Lib/Doktor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar.Lib
+{
+    public class Doktor:Calısan
+    {
+        public Branslar Brans { get; set; }
+
+    }
+}
