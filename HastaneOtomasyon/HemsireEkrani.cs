0fba56620ff583e04b212c681ee07fd11472ec8f
mucahitseba <mucahitseba@gmail.com>
2018-12-12 11:35:39 +0300

    Formlar eklendi

--- HastaneOtomasyon/HemsireEkrani.cs
+++ HastaneOtomasyon/HemsireEkrani.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace HastaneOtomasyon
 {
-    public partial class Form1 : Form
+    public partial class HemsireEkrani : Form
     {
-        public Form1()
+        public HemsireEkrani()
         {
             InitializeComponent();
         }
