268151b61ad1b1aa891931103f4548e7418a3947
Furkan Kılıç <furkankilicytu@gmail.com>
2018-12-12 15:22:13 +0300

    Branslar eklendi.

---
+++ Classlar.Lib/Branslar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar.Lib
+{
+    public enum Branslar
+    {
+        KBB,
+        Ortopedi,
+        Dahiliye,
+        Acil,
+        Göz,
+        İçhastalıkları,
+        KadınDoğum,
+        BeyinCerrahi
+    }
+    public enum Gorevler
+    {
+        Temizlik,
+        Muhasebe,
+        HastaBakım
+    }
+
+}
