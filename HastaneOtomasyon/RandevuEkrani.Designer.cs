9ca27dc7b8b38ca54a5e1b5ce92071b6a11afcf5
Furkan Kılıç <furkankilicytu@gmail.com>
2018-12-13 17:49:38 +0300

    saat ekranı

--- HastaneOtomasyon/RandevuEkrani.Designer.cs
+++ HastaneOtomasyon/RandevuEkrani.Designer.cs
@@ -28,323 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lstHastalar = new System.Windows.Forms.ListBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button21 = new System.Windows.Forms.Button();
-            this.button20 = new System.Windows.Forms.Button();
-            this.button16 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button19 = new System.Windows.Forms.Button();
-            this.button15 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button18 = new System.Windows.Forms.Button();
-            this.button14 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button17 = new System.Windows.Forms.Button();
-            this.button13 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.cmbServis = new System.Windows.Forms.ComboBox();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // lstHastalar
-            // 
-            this.lstHastalar.FormattingEnabled = true;
-            this.lstHastalar.Location = new System.Drawing.Point(25, 60);
-            this.lstHastalar.Name = "lstHastalar";
-            this.lstHastalar.Size = new System.Drawing.Size(153, 199);
-            this.lstHastalar.TabIndex = 1;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Control;
-            this.panel1.Controls.Add(this.button21);
-            this.panel1.Controls.Add(this.button20);
-            this.panel1.Controls.Add(this.button16);
-            this.panel1.Controls.Add(this.button12);
-            this.panel1.Controls.Add(this.button8);
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button19);
-            this.panel1.Controls.Add(this.button15);
-            this.panel1.Controls.Add(this.button11);
-            this.panel1.Controls.Add(this.button7);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button18);
-            this.panel1.Controls.Add(this.button14);
-            this.panel1.Controls.Add(this.button10);
-            this.panel1.Controls.Add(this.button6);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button17);
-            this.panel1.Controls.Add(this.button13);
-            this.panel1.Controls.Add(this.button9);
-            this.panel1.Controls.Add(this.button5);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.comboBox2);
-            this.panel1.Controls.Add(this.cmbServis);
-            this.panel1.Location = new System.Drawing.Point(264, 36);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(272, 290);
-            this.panel1.TabIndex = 2;
-            // 
-            // button21
-            // 
-            this.button21.Location = new System.Drawing.Point(164, 250);
-            this.button21.Name = "button21";
-            this.button21.Size = new System.Drawing.Size(75, 23);
-            this.button21.TabIndex = 3;
-            this.button21.Text = "Randevu Al";
-            this.button21.UseVisualStyleBackColor = true;
-            // 
-            // button20
-            // 
-            this.button20.Location = new System.Drawing.Point(177, 208);
-            this.button20.Name = "button20";
-            this.button20.Size = new System.Drawing.Size(45, 24);
-            this.button20.TabIndex = 2;
-            this.button20.Text = "14:45";
-            this.button20.UseVisualStyleBackColor = true;
-            // 
-            // button16
-            // 
-            this.button16.Location = new System.Drawing.Point(177, 178);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(45, 24);
-            this.button16.TabIndex = 2;
-            this.button16.Text = "13:45";
-            this.button16.UseVisualStyleBackColor = true;
-            // 
-            // button12
-            // 
-            this.button12.Location = new System.Drawing.Point(177, 148);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(45, 24);
-            this.button12.TabIndex = 2;
-            this.button12.Text = "11:45";
-            this.button12.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(177, 118);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(45, 24);
-            this.button8.TabIndex = 2;
-            this.button8.Text = "10:45";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(177, 88);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(45, 24);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "09:45";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button19
-            // 
-            this.button19.Location = new System.Drawing.Point(126, 208);
-            this.button19.Name = "button19";
-            this.button19.Size = new System.Drawing.Size(45, 24);
-            this.button19.TabIndex = 2;
-            this.button19.Text = "14:30";
-            this.button19.UseVisualStyleBackColor = true;
-            // 
-            // button15
-            // 
-            this.button15.Location = new System.Drawing.Point(126, 178);
-            this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(45, 24);
-            this.button15.TabIndex = 2;
-            this.button15.Text = "13:30";
-            this.button15.UseVisualStyleBackColor = true;
-            // 
-            // button11
-            // 
-            this.button11.Location = new System.Drawing.Point(126, 148);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(45, 24);
-            this.button11.TabIndex = 2;
-            this.button11.Text = "11:30";
-            this.button11.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(126, 118);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(45, 24);
-            this.button7.TabIndex = 2;
-            this.button7.Text = "10:30";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(126, 88);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(45, 24);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "09:30";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button18
-            // 
-            this.button18.Location = new System.Drawing.Point(75, 208);
-            this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(45, 24);
-            this.button18.TabIndex = 2;
-            this.button18.Text = "14:15";
-            this.button18.UseVisualStyleBackColor = true;
-            // 
-            // button14
-            // 
-            this.button14.Location = new System.Drawing.Point(75, 178);
-            this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(45, 24);
-            this.button14.TabIndex = 2;
-            this.button14.Text = "13:15";
-            this.button14.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(75, 148);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(45, 24);
-            this.button10.TabIndex = 2;
-            this.button10.Text = "11:15";
-            this.button10.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(75, 118);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(45, 24);
-            this.button6.TabIndex = 2;
-            this.button6.Text = "10:15";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(75, 88);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(45, 24);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "09:15";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button17
-            // 
-            this.button17.Location = new System.Drawing.Point(24, 208);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(45, 24);
-            this.button17.TabIndex = 2;
-            this.button17.Text = "14:00";
-            this.button17.UseVisualStyleBackColor = true;
-            // 
-            // button13
-            // 
-            this.button13.Location = new System.Drawing.Point(24, 178);
-            this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(45, 24);
-            this.button13.TabIndex = 2;
-            this.button13.Text = "13:00";
-            this.button13.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(24, 148);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(45, 24);
-            this.button9.TabIndex = 2;
-            this.button9.Text = "11:00";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(24, 118);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(45, 24);
-            this.button5.TabIndex = 2;
-            this.button5.Text = "10:00";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(24, 88);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(45, 24);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "09:00";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Doktorlar";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Servis";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(118, 40);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 0;
-            // 
-            // cmbServis
-            // 
-            this.cmbServis.FormattingEnabled = true;
-            this.cmbServis.Location = new System.Drawing.Point(118, 13);
-            this.cmbServis.Name = "cmbServis";
-            this.cmbServis.Size = new System.Drawing.Size(121, 21);
-            this.cmbServis.TabIndex = 0;
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(25, 382);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(511, 108);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // RandevuEkrani
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(617, 537);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.lstHastalar);
-            this.Name = "RandevuEkrani";
-            this.Text = "Randevu Ekranı";
-            this.Load += new System.EventHandler(this.RandevuEkrani_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.ResumeLayout(false);
+			this.lstHastalar = new System.Windows.Forms.ListBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button21 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.cmbServis = new System.Windows.Forms.ComboBox();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lstHastalar
+			// 
+			this.lstHastalar.FormattingEnabled = true;
+			this.lstHastalar.Location = new System.Drawing.Point(25, 60);
+			this.lstHastalar.Name = "lstHastalar";
+			this.lstHastalar.Size = new System.Drawing.Size(153, 199);
+			this.lstHastalar.TabIndex = 1;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.Control;
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.comboBox2);
+			this.panel1.Controls.Add(this.cmbServis);
+			this.panel1.Location = new System.Drawing.Point(264, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(272, 82);
+			this.panel1.TabIndex = 2;
+			// 
+			// button21
+			// 
+			this.button21.Location = new System.Drawing.Point(63, 297);
+			this.button21.Name = "button21";
+			this.button21.Size = new System.Drawing.Size(75, 23);
+			this.button21.TabIndex = 3;
+			this.button21.Text = "Randevu Al";
+			this.button21.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(56, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(50, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Doktorlar";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(56, 22);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(36, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Servis";
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(118, 40);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 21);
+			this.comboBox2.TabIndex = 0;
+			// 
+			// cmbServis
+			// 
+			this.cmbServis.FormattingEnabled = true;
+			this.cmbServis.Location = new System.Drawing.Point(118, 13);
+			this.cmbServis.Name = "cmbServis";
+			this.cmbServis.Size = new System.Drawing.Size(121, 21);
+			this.cmbServis.TabIndex = 0;
+			// 
+			// listView1
+			// 
+			this.listView1.Location = new System.Drawing.Point(25, 382);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(511, 108);
+			this.listView1.TabIndex = 3;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// panel2
+			// 
+			this.panel2.Location = new System.Drawing.Point(264, 112);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(272, 241);
+			this.panel2.TabIndex = 4;
+			// 
+			// RandevuEkrani
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(617, 537);
+			this.Controls.Add(this.button21);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.lstHastalar);
+			this.Name = "RandevuEkrani";
+			this.Text = "Randevu Ekranı";
+			this.Load += new System.EventHandler(this.RandevuEkrani_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
 
         }
 
@@ -354,29 +143,10 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox cmbServis;
         private System.Windows.Forms.Button button21;
-        private System.Windows.Forms.Button button20;
-        private System.Windows.Forms.Button button16;
-        private System.Windows.Forms.Button button12;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button19;
-        private System.Windows.Forms.Button button15;
-        private System.Windows.Forms.Button button11;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button18;
-        private System.Windows.Forms.Button button14;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button17;
-        private System.Windows.Forms.Button button13;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListView listView1;
-    }
+		private System.Windows.Forms.Panel panel2;
+	}
 }
 
--- HastaneOtomasyon/RandevuEkrani.cs
+++ HastaneOtomasyon/RandevuEkrani.cs
@@ -36,10 +36,46 @@ namespace HastaneOtomasyon
             }
 
         }
-
-        private void RandevuEkrani_Load(object sender, EventArgs e)
+		List<Button> seanslar = new List<Button>();
+		private void RandevuEkrani_Load(object sender, EventArgs e)
         {
             cmbServis.DataSource = Enum.GetValues(typeof(Branslar));
-        }
-    }
+
+
+			int dakika, saat, i;
+			int sayac = 0;
+			for (i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					Button btn = new Button();
+					btn.Name = "btn_" + (i * j).ToString();
+					btn.Location = new System.Drawing.Point(50 * j + 20, i * 40 + 40);
+					btn.Size = new System.Drawing.Size(50, 40);
+					seanslar.Add(btn);
+					this.panel2.Controls.Add(btn);
+
+				}
+			}
+
+			for (saat = 9; saat <= 16; saat++)
+			{
+				if (saat == 12)
+				{
+
+					continue;
+				}
+				for (dakika = 0; dakika <= 45; dakika += 15)
+				{
+					if (sayac != 20)
+					{
+						seanslar[sayac].Text = saat.ToString("00") + " : " + dakika.ToString("00");
+						sayac++;
+					}
+				}
+			}
+		}
+
+		
+	}
 }
